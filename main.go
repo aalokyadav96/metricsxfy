3989dfe32f64944daa59baa232538bb69870853d
Aalok Yadav <35891906+aalokyadav96@users.noreply.github.com>
2023-12-04 11:17:11 +0530

    Update CORS

--- main.go
+++ main.go
@@ -20,5 +20,5 @@ func Index(w http.ResponseWriter, r *http.Request) {
 }
 
 func enableCors(w *http.ResponseWriter) {
-(*w).Header().Set("Access-Control-Allow-Origin", "https://xfy.onrender.com/")
+(*w).Header().Set("Access-Control-Allow-Origin", "https://xfy.onrender.com")
 }
